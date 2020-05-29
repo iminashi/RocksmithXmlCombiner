8b77f0305ebf8de1f22017229e6c151e3226d464
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-05-29 19:11:32 +0300

    Use fold instead of mutable variable

--- RSXmlCombiner.FuncUI/ToolkitImporter.fs
+++ RSXmlCombiner.FuncUI/ToolkitImporter.fs
@@ -11,22 +11,26 @@ module ToolkitImporter =
     let private d4p1 = XNamespace.Get "http://schemas.datacontract.org/2004/07/RocksmithToolkitLib.DLCPackage.AggregateGraph"
 
     let private importOld (arrangements : IEnumerable<XElement>) templatePath title =
-        let mutable foundArrangements : Map<ArrangementType, string * string option> = Map.empty
-
-        for itemNode in arrangements do
+        let folder (state :  Map<ArrangementType, string * string option>) (itemNode : XElement) =
             let bonusArr = itemNode.Element(ad + "BonusArr").Value = "true"
             let mutable arrType = ArrangementType.Unknown
 
             // Try to read the arrangement type from route mask or the pluralized arrangement type value
             if ArrangementType.TryParse(itemNode.Element(ad + "RouteMask").Value, &arrType) || ArrangementType.TryParse(itemNode.Element(ad + "ArrangementType").Value + "s", &arrType) then
-                if not (bonusArr || foundArrangements.ContainsKey(arrType)) then
+                if not (bonusArr || state.ContainsKey(arrType)) then
                     let arrFn = Path.Combine(templatePath, itemNode.Element(ad + "SongXml").Element(d4p1 + "File").Value)
                     let baseTone = itemNode.Element(ad + "ToneBase").Value;
                 
-                    foundArrangements <- foundArrangements.Add(arrType, (arrFn, Some baseTone));
+                    state.Add(arrType, (arrFn, Some baseTone));
+                else
+                    state
+            else
+                state
 
-        foundArrangements
-            
+        arrangements |> Seq.fold folder Map.empty
+    
+    /// Imports the main arrangements from a Toolkit template.
+    /// Returns them in map: arrangement type to file name * possible base tone name.
     let import (fileName : string) =
         let templatePath = Path.GetDirectoryName(fileName)
         let xdoc = XElement.Load(fileName)
@@ -37,10 +41,8 @@ module ToolkitImporter =
         if arrangements.First().Element(ad + "ArrangementName") = null then
             importOld arrangements templatePath title
         else
-            // ArrangementType -> file name * base tone name
-            let mutable foundArrangements : Map<ArrangementType, string * string option> = Map.empty
-
-            for itemNode in arrangements do
+            // Map ArrangementType to file name * base tone name
+            let folder (state : Map<ArrangementType, string * string option>) (itemNode : XElement) =
                 let arrFn = Path.Combine(templatePath, itemNode.Element(ad + "SongXml").Element(d4p1 + "File").Value);
                 let arrType = ArrangementType.Parse(itemNode.Element(ad + "ArrangementName").Value);
     
@@ -48,9 +50,11 @@ module ToolkitImporter =
                 if itemNode.Element(ad + "Represent").Value = "true" then
                     let baseTone = itemNode.Element(ad + "ToneBase").Value;
     
-                    foundArrangements <- foundArrangements.Add(arrType, (arrFn, Some baseTone));
+                    state.Add(arrType, (arrFn, Some baseTone));
                 else if itemNode.Element(ad + "ArrangementName").Value = "Vocals" || itemNode.Element(ad + "ArrangementName").Value = "ShowLights" then
                     // TODO: JVocals
-                    foundArrangements <- foundArrangements.Add(arrType, (arrFn, None));
-    
-            foundArrangements
+                    state.Add(arrType, (arrFn, None));
+                else
+                    state
+
+            arrangements |> Seq.fold folder Map.empty
