c4990fcb0534e89c3c7a8de4f6981004b15539bb
Tapio Malmberg <tapio.malmberg@gmail.com>
2020-06-12 22:52:35 +0300

    Small improvement to the audio combiner

--- RSXmlCombiner.GUI/NAudioCombiner.fs
+++ RSXmlCombiner.GUI/NAudioCombiner.fs
@@ -7,7 +7,7 @@ module NAudioCombiner =
     open NAudio.Wave.SampleProviders
 
     /// Copies from the reader to the writer with the given amount in milliseconds trimmed from the start.
-    let addTrimmed (reader : WaveFileReader) (writer : WaveFileWriter) (trim : int) =
+    let private addTrimmed (reader : WaveFileReader) (writer : WaveFileWriter) (trim : int) =
         let bytesPerMillisecond = float reader.WaveFormat.AverageBytesPerSecond / 1000.0
         let startPos = int (float trim * bytesPerMillisecond)
         let trimStart = startPos - startPos % reader.WaveFormat.BlockAlign
@@ -20,9 +20,11 @@ module NAudioCombiner =
     /// Combines the audio files of the given tracks into the target file.
     let combineAudioFiles (tracks : Track list) (targetFile : string) =
         try
-            use formatGetter = new WaveFileReader(tracks.Head.AudioFile |> Option.get)
-            use writer = new WaveFileWriter(targetFile, formatGetter.WaveFormat)
-            for t in tracks do
+            use first = new WaveFileReader(tracks.Head.AudioFile |> Option.get)
+            use writer = new WaveFileWriter(targetFile, first.WaveFormat)
+            addTrimmed first writer 0
+
+            for t in tracks.Tail do
                 use fileReader = new WaveFileReader(t.AudioFile |> Option.get)
                 if not <| fileReader.WaveFormat.Equals(writer.WaveFormat) then
                     let error =
@@ -33,8 +35,7 @@ module NAudioCombiner =
                             (t.AudioFile |> Option.get |> Path.GetFileName)
                             currFormat.SampleRate currFormat.BitsPerSample currFormat.Channels
                     failwith error
-                let trimAmount = if t = tracks.Head then 0 else t.TrimAmount
-                addTrimmed fileReader writer trimAmount
+                addTrimmed fileReader writer t.TrimAmount
 
             sprintf "Audio files combined as %s" targetFile
         with
