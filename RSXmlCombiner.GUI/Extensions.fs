b8362482382d7c5cd78ebc5150956e4d1d93c4c0
Tapio Malmberg <tapio.malmberg@gmail.com>
2021-11-25 21:17:43 +0200

    Remove Array.updateAt

--- RSXmlCombiner.GUI/Extensions.fs
+++ RSXmlCombiner.GUI/Extensions.fs
@@ -16,12 +16,6 @@ module String =
     let notEmpty = String.IsNullOrEmpty >> not
 
 [<RequireQualifiedAccess>]
-module Array =
-    /// Returns a new array with the element at the given index changed to the new one.
-    let updateAt index newElem =
-        Array.mapi (fun i old -> if i = index then newElem else old)
-
-[<RequireQualifiedAccess>]
 module List =
     /// Returns a new list with the map function applied to the item at the given index.
     let mapAt index map list =
